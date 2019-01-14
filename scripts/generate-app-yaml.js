939ea4c019d2b31100a786ba0d19880e0c0d5770
Dominik Ferber <dominik.ferber@gmail.com>
2019-01-14 22:09:37 +0100

    add razzle

---
+++ scripts/generate-app-yaml.js
@@ -0,0 +1,19 @@
+// # All URLs ending in .gif .png or .jpg are treated as paths to
+// # static files in the static/ directory. The URL pattern is a
+// # regular expression, with a grouping that is inserted into the
+// # path to the file.
+// # - url: /(robots\.txt|favicon\.ico)$
+// #   static_files: build/public/\1
+// #   upload: static/.*\.(gif|png|jpg)$
+
+console.log(`runtime: nodejs10
+
+handlers:
+- url: /static
+  static_dir: build/public/static
+
+- url: /.*
+  secure: always
+  redirect_http_response_code: 301
+  script: auto
+`);
