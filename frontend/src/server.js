2719926b5c46b1d795361d4ced17a1244fce189a
Dominik Ferber <dominik.ferber@gmail.com>
2019-01-14 23:18:48 +0100

    swap express for koa & pretend not to be CI for build

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,47 +1,70 @@
-import App from './App';
-import React from 'react';
-import { StaticRouter } from 'react-router-dom';
-import express from 'express';
-import { renderToString } from 'react-dom/server';
+import App from "./App";
+import React from "react";
+import { StaticRouter } from "react-router-dom";
+import Koa from "koa";
+import { renderToString } from "react-dom/server";
+import serve from "koa-static";
+import Router from "koa-router";
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
-const server = express();
-server
-  .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', (req, res) => {
-    const context = {};
+// Initialize `koa-router` and setup a route listening on `GET /*`
+// Logic has been splitted into two chained middleware functions
+// @see https://github.com/alexmingoia/koa-router#multiple-middleware
+const router = new Router();
+router.get(
+  "/*",
+  async (ctx, next) => {
+    const routingContext = {};
+
     const markup = renderToString(
-      <StaticRouter context={context} location={req.url}>
+      <StaticRouter context={routingContext} location={ctx.url}>
         <App />
       </StaticRouter>
     );
 
-    if (context.url) {
-      res.redirect(context.url);
-    } else {
-      res.status(200).send(
-        `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${assets.client.css
-          ? `<link rel="stylesheet" href="${assets.client.css}">`
-          : ''}
-        ${process.env.NODE_ENV === 'production'
-          ? `<script src="${assets.client.js}" defer></script>`
-          : `<script src="${assets.client.js}" defer crossorigin></script>`}
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-    </body>
-</html>`
-      );
-    }
-  });
+    ctx.state.markup = markup;
+
+    return routingContext.url ? ctx.redirect(routingContext.url) : next();
+  },
+  ctx => {
+    ctx.status = 200;
+    ctx.body = `
+    <!doctype html>
+      <html lang="">
+      <head>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta charset="utf-8" />
+          <title>Welcome to Razzle + Koa</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          ${
+            process.env.NODE_ENV === "production"
+              ? `<script src="${assets.client.js}" defer></script>`
+              : `<script src="${assets.client.js}" defer crossorigin></script>`
+          }
+          <!-- Render the style tags gathered from the components into the DOM -->
+          ${ctx.state.styleTags}
+      </head>
+      <body>
+          <div id="root">${ctx.state.markup}</div>
+          <script>window.GRAPHQL_ENDPOINT = "${
+            process.env.GRAPHQL_ENDPOINT
+          }";</script> 
+      </body>
+    </html>`;
+  }
+);
+
+// Intialize and configure Koa application
+const server = new Koa();
+server
+  // `koa-helmet` provides security headers to help prevent common, well known attacks
+  // @see https://helmetjs.github.io/
+  // .use(helmet())
+  // .use(cookie())
+  // Serve static files located under `process.env.RAZZLE_PUBLIC_DIR`
+  .use(serve(process.env.RAZZLE_PUBLIC_DIR))
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 export default server;
