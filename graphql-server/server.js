d5ee011a21670bb2571de1fe51fe88cc647dbeaa
Dominik Ferber <dominik.ferber@gmail.com>
2019-02-11 19:27:22 +0100

    add health check to graphql-server

--- graphql-server/server.js
+++ graphql-server/server.js
@@ -1,8 +1,9 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
+const path = require("path");
 
 const server = new GraphQLServer({
-  typeDefs: "./schema.graphql",
+  typeDefs: path.resolve(__dirname, "schema.graphql"),
   resolvers: {
     Query: require("./resolvers/Query.js")
   },
@@ -11,10 +12,19 @@ const server = new GraphQLServer({
 
 const port = process.env.PORT || 4000;
 
-server.start(
-  {
-    port,
-    cors: { origin: true, credentials: true }
-  },
-  () => console.log(`GraphQL-Server is running on http://localhost:${port}`)
-);
+server
+  .use((req, res, next) => {
+    if (req.path.startsWith("/health")) {
+      res.statusCode = 200;
+      res.end();
+      return;
+    }
+    return next();
+  })
+  .start(
+    {
+      port,
+      cors: { origin: true, credentials: true }
+    },
+    () => console.log(`GraphQL-Server is running on http://localhost:${port}`)
+  );
